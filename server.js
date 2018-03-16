08030bb78aff9e97efd50c9e99ce22bd2e0f3a81
Alexei <axmetsh@gmail.com>
2018-03-16 21:33:31 +0300

    git test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 //app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 const multer  = require('multer');
 const upload = multer();
