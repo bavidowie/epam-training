e3fdad6dd288e0325d9256714ef2e107b01b845c
Alexei <axmetsh@gmail.com>
2018-03-16 21:18:19 +0300

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,195 @@
+$('.form').find('input, textarea').on('keyup blur focus', function(e) {
+
+    var $this = $(this);
+	var label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.removeClass('highlight');
+        }
+    } else if (e.type === 'focus') {
+        if ($this.val() === '') {
+            label.removeClass('highlight');
+        } else if ($this.val() !== '') {
+            label.addClass('highlight');
+        }
+    }
+
+});
+
+$('.tab a').on('click', function(e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+});
+
+
+var RegisterForm = document.getElementsByClassName('register')[0];
+var RegisterDate = document.getElementsByName('r_date')[0];
+var RegisterTime = document.getElementsByName('r_time')[0];
+var RegisterLogin = document.getElementsByName('r_login')[0];
+var RegisterEmail = document.getElementsByName('r_email')[0];
+var RegisterPass = document.getElementsByName('r_pass')[0];
+var RegisterPassRe = document.getElementsByName('r_pass_re')[0];
+var LoginForm = document.getElementsByClassName('login')[0];
+var LoginLogin = document.getElementsByName('l_login')[0];
+var LoginPass = document.getElementsByName('l_pass')[0];
+
+var tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+var dd = tomorrow.getDate();
+var mm = tomorrow.getMonth()+1; //January is 0!
+var yyyy = tomorrow.getFullYear();
+if(dd<10) {
+    dd = '0'+dd
+}
+if(mm<10) {
+    mm = '0'+mm
+} 
+tomorrow = yyyy + '-' + mm + '-' + dd;
+RegisterDate.min = tomorrow;
+RegisterDate.value = tomorrow;
+
+RegisterTime.value = '09:00';
+
+RegisterLogin.addEventListener('blur', validateRegisterLogin);
+RegisterLogin.addEventListener('focus', function() {
+	clearInputError(RegisterLogin);
+});
+RegisterEmail.addEventListener('blur', validateRegisterEmail);
+RegisterEmail.addEventListener('focus', function() {
+	clearInputError(RegisterEmail);
+});
+RegisterPass.addEventListener('blur', validateRegisterPass);
+RegisterPass.addEventListener('focus', function() {
+	clearInputError(RegisterPass);
+});
+RegisterPassRe.addEventListener('blur', validateRegisterPassRe);
+RegisterPassRe.addEventListener('focus', function() {
+	clearInputError(RegisterPassRe);
+});
+
+RegisterForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+	if (validateRegisterLogin && validateRegisterEmail && validateRegisterPass && validateRegisterPassRe) {
+		var RegisterData = new FormData(RegisterForm);
+		var xhr = new XMLHttpRequest();
+		xhr.addEventListener('loadend', function(res) {
+			console.log('Register ok');
+		});
+		xhr.open('POST', '/register');
+		xhr.send(RegisterData);
+	}
+});
+LoginForm.addEventListener('submit', function(e) {
+	e.preventDefault();
+	var LoginData = new FormData(LoginForm);
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener('loadend', function(res) {
+		console.log(this.responseText);
+	});
+	xhr.open('POST', '/signin');
+	xhr.send(LoginData);
+});
+
+
+function validateRegisterLogin () {
+	if (RegisterLogin.value == '') {
+		displayInputError(RegisterLogin, 'Login empty');
+		return false;
+	} else if (RegisterLogin.value.length < 4) {
+		displayInputError(RegisterLogin, 'Login too short, 4 characters minimum');
+		return false;
+	} else if (RegisterLogin.value.length > 64) {
+		displayInputError(RegisterLogin, 'Login too long, 64 characters maximum');
+		return false;
+	} else if (RegisterLogin.value.match(/[^a-zA-Z0-9_]/) !== null) {
+		displayInputError(RegisterLogin, 'Login can only contain letters, numbers and underscores');
+		return false;
+	}
+	var RegisterData = new FormData(RegisterForm);
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener('loadend', function() {
+		if (this.responseText === '1') {
+			displayInputError(RegisterLogin, 'Login already taken');
+			return false;
+		} else {
+			clearInputError(RegisterLogin);
+			return true;
+		}
+	});
+	xhr.open('POST', '/logincheck');
+	xhr.send(RegisterData);
+}
+function validateRegisterEmail () {
+	if (RegisterEmail.value == '') {
+		displayInputError(RegisterEmail, 'Email empty');
+		return false;
+	} else if (RegisterEmail.value.match(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i) === null) {
+		displayInputError(RegisterEmail, 'Email invalid');
+		return false;
+	}
+	var RegisterData = new FormData(RegisterForm);
+	var xhr = new XMLHttpRequest();
+	xhr.addEventListener('loadend', function() {
+		if (this.responseText === '1') {
+			displayInputError(RegisterEmail, 'Email already registered');
+			return false;
+		} else {
+			clearInputError(RegisterEmail);
+			return true;
+		}
+	});
+	xhr.open('POST', '/emailcheck');
+	xhr.send(RegisterData);
+}
+function validateRegisterPass () {
+	if (RegisterPass.value.length < 5) {
+		displayInputError(RegisterPass, 'Password must be at least 5 symbols long');
+		return false;
+	} else if (RegisterPass.value.length > 64) {
+		displayInputError(RegisterPass, 'Password too long, 64 characters maximum');
+		return false;
+	}
+	return true;
+}
+function validateRegisterPassRe () {
+	if (RegisterPass.value !== RegisterPassRe.value) {
+		displayInputError(RegisterPassRe, 'Passwords do not match');
+		return false;
+	}
+	return true;
+}
+
+
+function displayInputError (InputField, ErrorText) {
+	clearInputError(RegisterLogin);
+	var ErrorBlock = document.createElement('div');
+	ErrorBlock.classList.add('errormsg');
+	var ErrorTextNode = document.createTextNode(ErrorText);
+	ErrorBlock.appendChild(ErrorTextNode);
+	InputField.parentNode.appendChild(ErrorBlock);
+}
+function clearInputError (InputField) {
+	var LastSibling = InputField.parentNode.children[InputField.parentNode.children.length-1];
+	if (LastSibling.classList.contains('errormsg')) {
+		LastSibling.remove();
+	}
+}
---
+++ server.js
@@ -0,0 +1,72 @@
+const path = require('path');
+//const logger = require('morgan');
+
+const express = require('express');
+const app = express();
+//app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const multer  = require('multer');
+const upload = multer();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://openodeapp:qwerty123@ds259258.mlab.com:59258/training');
+const userSchema = mongoose.Schema({
+	login: String,
+	email: String,
+	pass: String
+});
+const registered_user = mongoose.model('registered_user', userSchema);
+
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+
+
+app.post('/logincheck', upload.array(), function(req, res) {
+	registered_user.find({login: req.body.r_login}, function (err, user_found) {
+		if (err) return console.error(err);
+		user_found.length > 0 ? res.send('1') : res.send('0');
+	});
+});
+app.post('/emailcheck', upload.array(), function(req, res) {
+	registered_user.find({email: req.body.r_email}, function (err, user_found) {
+		if (err) return console.error(err);
+		user_found.length > 0 ? res.send('1') : res.send('0');
+	});
+});
+
+app.post('/register', upload.array(), function(req, res) {
+	var UserNew = new registered_user({
+		login: req.body.r_login,
+		email: req.body.r_email,
+		pass: bcrypt.hashSync(req.body.r_pass, salt)
+	});
+	UserNew.save(function (err, UserNew) {
+		if (err) return console.error(err);
+		res.send();
+	});
+});
+
+app.post('/signin', upload.array(), function(req, res) {
+	var Login = req.body.l_login;
+	var Pass = req.body.l_pass;
+	registered_user.find({$or:[{login: Login},{email: Login}]}, function(err, found) {
+		if (err) return console.error(err);
+		if (found.length > 0) {
+			console.log(found[0]);
+			if (bcrypt.compareSync(Pass, found[0].pass)) {
+				res.writeHead(302, {
+				  'Location': 'account.html'
+				});
+				res.send('login ok');
+			}
+		}
+	});
+	res.send('login error');
+});
+
+app.listen(80, (err) => {
+	if (!err) {
+		console.log('server is listening on 80');
+	}
+});
