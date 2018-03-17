e53ffca96f86223c442095a4dfb4267e5e3f56de
Alexei <axmetsh@gmail.com>
2018-03-17 16:21:06 +0300

    test

--- public/js/index.js
+++ public/js/index.js
@@ -103,7 +103,7 @@ LoginForm.addEventListener('submit', function(e) {
 	var LoginData = new FormData(LoginForm);
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener('loadend', function(res) {
-		console.log(this.responseText);
+		console.log(res);
 	});
 	xhr.open('POST', '/signin');
 	xhr.send(LoginData);
