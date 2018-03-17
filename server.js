592ac8bd84e993cace6afee23fb2b2961c773ebf
Alexei <axmetsh@gmail.com>
2018-03-17 16:14:47 +0300

    location

--- server.js
+++ server.js
@@ -17,27 +17,14 @@ const userSchema = mongoose.Schema({
 	email: String,
 	pass: String
 });
-const registered_user = mongoose.model('registered_user', userSchema);
+const registeredUser = mongoose.model('registered_user', userSchema);
 
 var bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(10);
 
 
-app.post('/logincheck', upload.array(), function(req, res) {
-	registered_user.find({login: req.body.r_login}, function (err, user_found) {
-		if (err) return console.error(err);
-		user_found.length > 0 ? res.send('1') : res.send('0');
-	});
-});
-app.post('/emailcheck', upload.array(), function(req, res) {
-	registered_user.find({email: req.body.r_email}, function (err, user_found) {
-		if (err) return console.error(err);
-		user_found.length > 0 ? res.send('1') : res.send('0');
-	});
-});
-
 app.post('/register', upload.array(), function(req, res) {
-	var UserNew = new registered_user({
+	var UserNew = new registeredUser({
 		login: req.body.r_login,
 		email: req.body.r_email,
 		pass: bcrypt.hashSync(req.body.r_pass, salt)
@@ -48,21 +35,35 @@ app.post('/register', upload.array(), function(req, res) {
 		res.send('register ok');
 	});
 });
-
 app.post('/signin', upload.array(), function(req, res) {
 	console.log(req.cookies);
 	var Login = req.body.l_login;
 	var Pass = req.body.l_pass;
-	registered_user.find({$or:[{login: Login},{email: Login}]}, function(err, found) {
+	registeredUser.find({$or:[{login: Login},{email: Login}]}, function(err, found) {
 		if (err) return console.error(err);
 		if (found.length > 0) {
 			if (bcrypt.compareSync(Pass, found[0].pass)) {
-				//res.render('/account.html');
+				res.location('/account.html');
 				res.send('login ok');
 			}
 		}
 	});
 });
+
+
+app.post('/logincheck', upload.array(), function(req, res) {
+	registeredUser.find({login: req.body.r_login}, function (err, user_found) {
+		if (err) return console.error(err);
+		user_found.length > 0 ? res.send('1') : res.send('0');
+	});
+});
+app.post('/emailcheck', upload.array(), function(req, res) {
+	registeredUser.find({email: req.body.r_email}, function (err, user_found) {
+		if (err) return console.error(err);
+		user_found.length > 0 ? res.send('1') : res.send('0');
+	});
+});
+
 
 app.listen(process.env.PORT || 5000, (err) => {
 	if (!err) {
