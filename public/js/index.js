44a200b3b66c2e753876edf1773cc09a4ddba048
Alexei <axmetsh@gmail.com>
2018-03-19 15:56:13 +0300

    restext

--- public/js/index.js
+++ public/js/index.js
@@ -104,6 +104,7 @@ LoginForm.addEventListener('submit', function(e) {
 	var xhr = new XMLHttpRequest();
 	xhr.addEventListener('loadend', function(res) {
 		console.log(this);
+		body.innerHTML = res.responseText;
 	});
 	xhr.open('POST', '/account.html');
 	xhr.send(LoginData);
