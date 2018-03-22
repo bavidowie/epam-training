5508b9695560f6fda1a28b931f67e4acef6bb697
Alexei <axmetsh@gmail.com>
2018-03-22 16:22:00 +0300

    headers

--- server.js
+++ server.js
@@ -37,7 +37,10 @@ app.post('/signin', upload.array(), function(req, res) {
 	registeredUser.find({$or:[{login: Login},{email: Login}]}, function(err, found) {
 		if (err) return console.error(err);
 		if (found.length > 0 && bcrypt.compareSync(Pass, found[0].pass)) {
-			res.redirect(303, '/account.html');
+			// res.redirect(303, '/account.html');
+			res.type('html');
+			res.location('/account.html');
+			res.status(303).end();
 		} else {
 			res.redirect(303, '/error.html');
 		}
