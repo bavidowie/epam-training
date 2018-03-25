f0b53552d5923ab7d2cbf81e0201f672183740a6
Alexei <axmetsh@gmail.com>
2018-03-25 22:48:34 +0300

    config

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
 
 const multer  = require('multer');
 const upload = multer();
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://openodeapp:qwerty123@ds259258.mlab.com:59258/training');
