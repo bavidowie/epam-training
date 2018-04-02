e54f2e7eb0a144444c45d04624828910ac54e4b5
Alexei <axmetsh@gmail.com>
2018-04-02 21:27:57 +0300

    sd

--- public/js/account.js
+++ public/js/account.js
@@ -2,8 +2,8 @@ const coursesTable = document.getElementsByClassName('courses')[0];
 
 var xhr = new XMLHttpRequest();
 xhr.addEventListener('loadend', function() {
-	console.log(this);
-	coursesTable.innerHTML = JSON.parse(this.responseText);
+	courses = JSON.parse(this.responseText);
+	console.log(courses);
 });
 xhr.open('GET', '/courses');
 xhr.send();
