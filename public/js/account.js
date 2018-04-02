a48f7caa6a23374f6df3b696ccbcad498e894eb2
Alexei <axmetsh@gmail.com>
2018-04-02 21:49:52 +0300

    d

--- public/js/account.js
+++ public/js/account.js
@@ -3,7 +3,14 @@ const coursesTable = document.getElementsByClassName('courses')[0];
 var xhr = new XMLHttpRequest();
 xhr.addEventListener('loadend', function() {
 	courses = JSON.parse(this.responseText);
-	console.log(courses);
+	courses.map(function(val, i) {
+		let courseDiv = document.createElement('div');
+		let courseDetails = document.createTextNode(val);
+		let cancelCourseBtn = document.createElement('input').setAttribute('type', 'button');
+		cancelCourseBtn.addEventListener('click', () => alert(val));
+		courseDiv.appendChild(courseDetails)
+				 .appendChild(cancelCourseBtn);
+	});
 });
 xhr.open('GET', '/courses');
 xhr.send();
